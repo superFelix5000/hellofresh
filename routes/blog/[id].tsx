5109fbf61f003302ee5d373105236f6db160b4ac
Felix <fwest@posteo.de>
2023-02-19 10:05:48 +0200

    basic rendering of post

---
+++ routes/blog/[id].tsx
@@ -0,0 +1,24 @@
+import { Head } from "$fresh/runtime.ts";
+import { Post } from "../../utils/posts.ts"
+
+const post: Post = {
+  id: "hello",
+  title: "Hello World",
+  publishAt: new Date(),
+  snippet: "my first post",
+  content: "aslkdjalskjd"
+}
+
+export default function Home() {
+  return (
+    <>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="text-gray-600 mt-12">{post.publishAt.toLocaleDateString()}</p>
+        <h1 class="text-5xl mt-2 font-bold">{post.title}</h1>
+        <div class="mt-12">
+          {post.content}
+        </div>
+      </div>
+    </>
+  );
+}
