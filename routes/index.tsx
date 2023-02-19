cc643051aa2ca80e144be117d41fc265de9cf73d
Felix <fwest@posteo.de>
2023-02-19 17:26:44 +0200

    rendering the list of posts

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,36 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { listPosts, Post } from "../utils/posts.ts";
 
-export default function Home() {
+export const handler: Handlers<Post[]> = {
+   async GET(_req, ctx) {
+    const posts = await listPosts();
+    return ctx.render(posts);
+   }
+}
+
+export default function Home(props: PageProps<Post[]>) {
+  const posts = props.data;
   return (
     <>
-      <div class="p-4 mx-auto max-w-screen-md">
-        Hello World.
+      <div class="px-4 mx-auto max-w-screen-md">
+        <h1 class="text-5xl mt-12 font-bold">Felix' Blog</h1>
+        <ul class="mt-8">
+          {posts.map((post) => <PostEntry post = {post} />)}
+        </ul>
       </div>
     </>
   );
+}
+
+function PostEntry(props: {post: Post}) {
+  const post = props.post;
+  return <li class="border-t">
+    <a href={`/blog/${post.id}`} class="flex py-2 gap-4 group">
+      <div>{post.publishAt.toLocaleDateString()}</div>
+      <div>
+        <h2 class="font-bold text-xl group-hover:underline">{post.title}</h2>
+        <p class="text-gray-600">{post.snippet}</p>
+      </div>
+    </a>  
+  </li>;
 }
