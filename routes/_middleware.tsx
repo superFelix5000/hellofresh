8a4c6a235f9b8ad36f084b164740069c9210e3da
Felix <fwest@posteo.de>
2023-02-23 22:11:12 +0200

    form handling in settings

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -1,9 +1,16 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { parse } from "https://esm.sh/accept-language-parser@1.5.0";
 import { State } from "../utils/state.ts";
+import {getCookies} from "$std/http/cookie.ts";
 
 export function handler(req: Request, ctx: MiddlewareHandlerContext<State>) {
   ctx.state.locales = [];
+
+  const cookies = getCookies(req.headers);
+  if (cookies.locale) {
+    ctx.state.locales.push(cookies.locale);
+  }
+
   const langs = parse(req.headers.get("accept-language") || undefined);
   for (const lang of langs) {
     let locale = lang.code;
@@ -13,7 +20,9 @@ export function handler(req: Request, ctx: MiddlewareHandlerContext<State>) {
     ctx.state.locales.push(locale);
   }
 
-  if (ctx.state.locales.length === 0) ctx.state.locales.push("en");
+  if (ctx.state.locales.length === 0) {
+    ctx.state.locales.push("en");
+  }
 
   return ctx.next();
 }
--- routes/settings.tsx
+++ routes/settings.tsx
@@ -1,10 +1,30 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import {setCookie} from "$std/http/cookie.ts";
 import { State } from "../utils/state.ts";
 
 export const handler: Handlers<State, State> = {
   async GET(_req, ctx) {
     return ctx.render(ctx.state);
   },
+
+  async POST(req) {
+    const form = await req.formData();
+    const locale = form.get("locale");
+    const headers = new Headers({
+        Location: "/settings"
+    });
+    if (typeof(locale) === "string") {
+        setCookie(headers, {
+            name: "locale",
+            value: locale,
+            maxAge: 60 * 60 * 24 * 356
+        });
+    }
+    return new Response("", {
+        status: 303,
+        headers
+    });
+  }
 };
 
 export default function SettingsPage(props: PageProps<State>) {
@@ -15,6 +35,11 @@ export default function SettingsPage(props: PageProps<State>) {
       <p class="mt-4">
         Your current locale is <b>{locales[0]}</b>.
       </p>
+      <form method="post" class="space-x-2">
+        <label htmlFor="locale">Locale</label>
+        <input type="text" name="locale" id="locale" class="border px-2 py-1"/>
+        <button type="submit" class="px-2 py-2 bg-blue(500 hover:700 disabled:200) text-white font-medium">Save</button>
+      </form>
     </div>
   );
 }
